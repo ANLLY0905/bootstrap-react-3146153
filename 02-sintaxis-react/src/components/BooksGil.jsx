f27ee766acd1e12963545bf28f5ea192a61fca1b
Anlly <anllyotalvarosoto@gmail.com>
2025-10-22 13:23:53 -0500

    Props React

---
+++ 02-sintaxis-react/src/components/BooksGil.jsx
@@ -0,0 +1,9 @@
+export function BooksGil ({libro, nombre, descri}) {
+    return (
+        <>
+        <img src={libro} alt="" width="180" />
+        <h2>{nombre}</h2>
+        <p>{descri}</p>
+        </>
+    )
+}
