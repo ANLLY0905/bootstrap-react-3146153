6d4f99c3ecaaca8f58aa54b1d4c2d89488835a53
ANLLY0905 <anllyotalvarosoto@gmail.com>
2025-12-10 13:29:46 -0500

    Avance

--- 04-visumbrio/src/App.jsx
+++ 04-visumbrio/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { SecVideo } from './components/SecVideo'
+import { Hero } from './components/Hero'
 
 export const App = () => {
   return (
     <>
-    
+     <div className="header">
+
+      <div className="text-center pt-2">
+        <img 
+          src="./src/assets/LogotipoV.png"   
+          alt="Visumbrío Logo"
+          className="logo-img"
+        />
+      </div>
+
+      <nav className="nav justify-content-center mt-3 menu-principal">
+        <a className="nav-link" href="#">Inicio</a>
+        <a className="nav-link" href="#">Cómic</a>
+        <a className="nav-link" href="#">Integrantes</a>
+        <a className="nav-link" href="#">Tienda</a>
+      </nav>
+
+    </div>
+    <Hero/>
+    <SecVideo/>
     </>
+
   )
 }
---
+++ 04-visumbrio/src/components/SecVideo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const SecVideo = () => {
+  return (
+    <section className='container text-center my-5'>
+        <h3 className='mb-4'>Conoce nuestro cómic</h3>
+
+    </section>
+  )
+}
